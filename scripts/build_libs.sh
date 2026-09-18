fd4b3e2a5ac57fd54e6de9e24810df416582eb32
blockawa <blockawa@users.noreply.github.com>
2026-09-18 22:32:43 +0800

    FFmpegPlugin移植到NativeLibPlugin (FCL框架)

---
+++ scripts/build_libs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+export ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT:-/data/data/com.termux/files/home/android-sdk}
+export ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT:-/data/data/com.termux/files/home/android-sdk/ndk/29.0.14033849}
+if [[ "$PWD" == *scripts ]]; then
+   cd ..
+fi
+if [ ! -e ./ffmpeg-kit ]; then
+  echo "No ffmpeg-kit. Did you run setup_environ.sh first?"
+  exit 1
+fi
+cd ffmpeg-kit
+./android.sh --api-level=26 --enable-gpl --enable-shine --enable-x264 --enable-x265 --enable-libtheora --enable-opus --enable-libvorbis --enable-twolame
+function copy_libs {
+   local DEST="../app/src/main/jniLibs/$1"
+   if [ -e "$DEST" ]; then
+      rm -r "$DEST"/*
+   else
+      mkdir -p "$DEST"
+   fi
+   cp -t "$DEST" prebuilt/$2/ffmpeg/bin/* prebuilt/$2/ffmpeg/lib/*
+   mv "$DEST/ffmpeg" "$DEST/libffmpeg.so"
+   mv "$DEST/ffprobe" "$DEST/libffprobe.so"
+   cp android/libs/$1/libc++_shared.so "$DEST/"
+}
+
+if [ ! -e ../app/src/main/jniLibs ]; then
+      mkdir -p ../app/src/main/jniLibs
+fi
+
+copy_libs armeabi-v7a android-arm-neon
+copy_libs arm64-v8a android-arm64
+copy_libs x86 android-x86
+copy_libs x86_64 android-x86_64
