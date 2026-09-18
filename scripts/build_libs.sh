4087661478f20e1ac28ef1e517cea040947ccc1c
blockawa <blockawa@users.noreply.github.com>
2026-09-18 22:36:39 +0800

    fix: NDK路径自动检测 + 升级NDK 27.1.12297006

--- scripts/build_libs.sh
+++ scripts/build_libs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT:-/data/data/com.termux/files/home/android-sdk}
-export ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT:-/data/data/com.termux/files/home/android-sdk/ndk/27.0.12077973}
+export ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT:-$ANDROID_HOME}
+export ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT:-$ANDROID_HOME/ndk/27.1.12297006}
 if [[ "$PWD" == *scripts ]]; then
    cd ..
 fi
